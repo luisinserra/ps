0105cf0953523af1c9ff4856c3736abd03c76691
Luis Inserra <luisinserra@yahoo.com.br>
2018-05-24 14:03:37 -0300

    Trazendo os contatos...

--- www/js/jsAtendimentos.js
+++ www/js/jsAtendimentos.js
@@ -10,23 +10,68 @@ apkCliente.service('servico', function(){
 	}
 
 	this.carregaClientes=function(parm, $http, $scope){
-		var retorno='';
-		$http.get("https://www.w3schools.com/angular/customers_mysql.php")
+		var retorno={};
+		var conta=0;
+		var ok=new Boolean(false);
+		var promise=$http.get("https://www.w3schools.com/angular/customers_mysql.php")
 		.then(function (response){
 			retorno=response.data.records;
 			$scope.retorno=retorno;
 		});
 		return retorno;
 	}
+
+	this.displayInfo=function($scope){
+		var dados=$scope.retorno;
+		console.log("trabalhando...");
+		for (var i = 0; i < dados.length; i++) {
+			var pessoa=dados[i];
+			var nome=pessoa.Name;
+			var pais=pessoa.Country;
+			var cidade=pessoa.City;
+			console.log("Tratando "+nome+" de "+cidade+'-'+pais);
+		}
+		$scope.pessoas=$scope.retorno;
+	}
 });
 
-apkCliente.controller('clienteCtrl', function($scope, servico, $http){
+apkCliente.controller('clienteCtrl', function($scope, servico, $http, $interval){
 	console.log("Controller trabalhando");
 	$scope.bc=goBuscaCliente;
 
-	$scope.trazListaClientes=function(){
+	var resposta=servico.carregaClientes('kk',$http, $scope);
+	var n=0;
+	var conta=0;
+	$scope.intervalPromise = $interval(function(){
+		try {
+			n=$scope.retorno.length;
+		} catch(e){}
+          if (n > 0 || conta > 20)
+          {
+              console.log($scope.retorno);
+              servico.displayInfo($scope);
+              $interval.cancel($scope.intervalPromise);
+          }
+          conta++;
+      }, 100);
+
+
+	$scope.reLeitura=function(){
 		var resposta=servico.carregaClientes('kk',$http, $scope);
-		var inter = setInterval(function(){console.log($scope.retorno);clearInterval(inter)}, 5000);
+		var n=0;
+		var conta=0;
+		$scope.intervalPromise = $interval(function(){
+			try {
+				n=$scope.retorno.length;
+			} catch(e){}
+	        if (n > 0 || conta > 20)
+	        {
+	              console.log($scope.retorno);
+	              $interval.cancel($scope.intervalPromise);
+	        }
+	        conta++;
+    	}, 100);
+    	return $scope.retorno;
 	}
 });
 function goBuscaCliente(){
@@ -36,7 +81,7 @@ function goBuscaCliente(){
 }
 function rodar(){
 	console.log('vai ir...');
-	vaiNome('foi');
+	vaiNome2('foi');
 	var inter=getMemo('inter');
 	clearInterval(inter);
 }
@@ -51,6 +96,3 @@ function vaiNome2(parm){
 	co.nome=parm;
 	co.$apply();
 }
-
-var inter = setInterval(function(){kk=angular.element(document.getElementById('idScopo'));sc=kk.scope();sc.trazListaClientes();clearInterval(inter)}, 5000);
-//sc.trazListaClientes();
