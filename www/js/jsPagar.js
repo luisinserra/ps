b6919591ae38072e0f97a7381fade0fd6c136611
Luis Inserra <luisinserra@yahoo.com.br>
2018-05-31 20:27:48 -0300

    Buscando fornecedor

--- www/js/jsPagar.js
+++ www/js/jsPagar.js
@@ -1,13 +1,21 @@
-var apkPagar = angular.module('pagarApk', ['']);
+var apkPagar = angular.module('pagarApk', ['ngRoute']);
+apkPagar.config(function($routeProvider){
+	$routeProvider
+    .when("/", {
+        templateUrl : "templates/pagar/rowPesq.html"
+    })
+    .when("/listaFornecedores", {
+        templateUrl : "templates/pagar/listaFornecedores.html"
+    });
+});
 apkPagar.service('servicosPesquisa', function(){
-	console.log('serviço rodando');
 	this.goBuscaForn=function(){
-		var escopo=getScopo('topo');var $http=escopo.http;
+		var escopo=getScopo('divPesqPagar');var $http=escopo.http;
 		var parm=escopo.tForn;
 		if (parm == undefined){
 			parm="";
 		}
-		var urle="http://clevermidia.com.br/printsource/chooseNomeFornecedorWS.html?Funcao=";
+		var urle="http://localhost:8080/geosmarty/chooseNomeFornecedorWS.html?Funcao=";
 		var parms="&login="+window.localStorage.getItem('userLogin');
     	parms+="&senha="+window.localStorage.getItem('senha');
 	    parms+="&parm="+parm;
@@ -16,22 +24,50 @@ apkPagar.service('servicosPesquisa', function(){
 	    var retorno='';
 	    var promise=$http.get(urle)
 		.then(function (response){
-			retorno={"erro":"","codigo":"200"};
+			retorno={"erro":"","codigo":""};
 			escopo.retorno=retorno;
-			alert("Retornou dados");
+			retorno=response.data;
+			var fornecedores=retorno.registros;
+			var tInicial=290;
+			for (var i = 0; i < fornecedores.length; i++) {
+				fornecedores[i].tope=tInicial;
+				tInicial+=50;
+			}
+			escopo.fornecedores=fornecedores;
+			var fundo=160;
+			var n=fornecedores.length;
+			n--;
+			fundo+=n*50;
+			document.getElementById('canto').style.height=fundo+'px';
+			location.href="pagar.html#!/listaFornecedores";
 		}, function deuRuim(response) {
-        	escopo.retorno = '{"erro":"Falhou","codigo":"'+response.status+'"}';
+			retorno={"erro":"Falhou","codigo":response.status};
+        	escopo.retorno = retorno;
         	console.log("retornou erro");
         	var msg="Erro: "+retorno.erro+"\nCódigo: "+retorno.codigo;
         	alert(msg);
     	});
 		return retorno;
 	}
+
+	this.pegaFornecedor=function(codigo){
+		var escopo=getScopo('divPesqPagar');
+		var fornecedores=escopo.fornecedores;
+		putMemo('Angular','divPesqPagar');
+		var fornecedor=getJsonByCampo(fornecedores,"id",codigo);
+		alert("Escolheu "+fornecedor.nome);
+		escopo.tForn=fornecedor.nome;
+		escopo.fornecedor=fornecedor;
+		location.href="pagar.html#!";
+	}
 });
 apkPagar.controller('pagarCtrl', function($scope, servicosPesquisa, $http){
-	console.log("Controller acionada");
 	$scope.http=$http;
 	$scope.goBuscaForn=servicosPesquisa.goBuscaForn;
 	$scope.indice=0;
-	console.log("indice setado");
+	$scope.pegaFornecedor=servicosPesquisa.pegaFornecedor;
+	$scope.filtroForn='';
+	$scope.filtroInicio='';
+	$scope.filtroFim='';
 });
+
